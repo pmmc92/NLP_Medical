141b69048f9bbf2b4cfdc85fb4a9a9783899774e
pmmc92 <pmmc92@users.noreply.github.com>
2022-02-07 11:40:20 +0000

    work

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import spacy
+from spacy import displacy
+
+def text_function(texto):
+    nlp=spacy.load("output/model-last/")
+    doc=nlp(texto)
+    colors = {"Estado" : "#5F9EA0", "Terapêutica" : "#00FF00","RAM" : "#FFA07A","MCDT" : "#FF00FF", "Dose" : "#696969", "Posologia" : "#E6E6FA"}
+    options = {"colors" : colors}
+    return displacy.render(doc,style="ent",jupyter="True", options = options)
+
+def main():
+    st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+    st.title('Text Analysis using Spacy Textblob')
+    st.markdown('Type a sentence in the below text box and choose the desired option in the adjacent menu.')
+    texto = st.text_input("Enter the sentence")
+
+    if st.button("teste"):
+        text_function(texto)
+if __name__ == "__main__":
+    main()
