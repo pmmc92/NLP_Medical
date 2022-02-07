72b4702709bed8e03661dda0aa8a19402e71ea07
pmmc92 <pmmc92@users.noreply.github.com>
2022-02-07 12:13:40 +0000

    work

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import spacy
 from spacy import displacy
 
 def text_function(texto):
-    nlp=spacy.load("output/model-last/")
-    doc=nlp(texto)
+    nlp = spacy.load("output/model-last/")
+    doc = nlp(texto)
     colors = {"Estado" : "#5F9EA0", "Terapêutica" : "#00FF00","RAM" : "#FFA07A","MCDT" : "#FF00FF", "Dose" : "#696969", "Posologia" : "#E6E6FA"}
     options = {"colors" : colors}
-    html = displacy.render(doc,style="ent", jupyter = False,options=options)
-	st.markdown(html, unsafe_allow_html=True)
+    html = displacy.render(doc,style = "ent", jupyter = False,options = options)
+    st.markdown(html, unsafe_allow_html = True)
 
 def main():
-    st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+    st.set_page_config(layout = 'wide', initial_sidebar_state = 'expanded')
     st.title('Text Analysis using Spacy Textblob')
     st.markdown('Type a sentence in the below text box and choose the desired option in the adjacent menu.')
     texto = st.text_input("Enter the sentence")
